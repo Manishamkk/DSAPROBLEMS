afd563ca6c3f6179395bfb80e812cb64d23fe8d9
Manishamkk <manishakadam72217@gmail.com>
2026-04-12 16:28:27 +0530

    Butterfly Pattern

---
+++ .vscode/ButterFly.java
@@ -0,0 +1,21 @@
+public class ButterFly {
+ 
+
+    public static void main(String[] args) {
+         int n =5;
+       for(int i = 1 ;i<=n;i++){
+           for(int j = 1;j<=i;i++){
+            System.out.print("* ");
+           }
+           for(int j = 1 ;j<=2*(n-i);j++){
+            System.out.print(" ");
+           }
+   for(int j =1;j<i;j++){
+    System.out.print("*");
+   }
+
+System.out.println(); 
+        }
+    }
+
+}
